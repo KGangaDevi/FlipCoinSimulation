b7e48fdfe1e1d249751b3e837da01a2fbb334ebf
gangadevi <kakarlagangadevi@gmail.com>
2020-07-13 00:00:35 +0530

    Flipping a coin multiple times, displays number of times HEAD and TAIL Won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,17 @@
 #!/bin/bash -x
 echo "Problem displays winner Heads or Tails"
+read -p "Enter fliptimes" fliptime
+headsCount=0
+tailsCount=0
+for ((i=1;i<=fliptime;i++))
+do
 flipcoin=$((RANDOM%2))
 if [[ $flipcoin -eq 1 ]]
 then
-	echo "Heads"
+	((headsCount++))
 else
-	echo "Tails"
+	((tailsCount++))
 fi
+done
+echo "Number of times Heads Won" $headsCount
+echo "Number of times Tails Won" $tailsCount
