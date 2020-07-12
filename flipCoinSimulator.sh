20a6f6e3649d320ab465ce989238a29d519cf941
gangadevi <kakarlagangadevi@gmail.com>
2020-07-13 00:55:28 +0530

    Display win or Tie

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 echo "Problem displays winner Heads or Tails"
-read -p "Enter fliptimes" fliptime
+#read -p "Enter fliptimes" fliptime
 headsCount=0
 tailsCount=0
-for ((i=1;i<=fliptime;i++))
+while [[ $headsCount -le 21 || $tailsCount -le 21 ]]
 do
 flipcoin=$((RANDOM%2))
 if [[ $flipcoin -eq 1 ]]
@@ -12,6 +12,18 @@ then
 else
 	((tailsCount++))
 fi
+if [[ $headsCount -eq $tailsCount ]]
+then
+echo "TIE"
+elif [[ $headsCount -eq 21 ]]
+then
+count=$(($headsCount-$tailsCount))
+echo "Heads Won by " $count
+elif [[ $tailsCount -eq 21 ]]
+then
+count=$(($tailsCount-$headsCount))
+echo "Tails Won by " $count
+fi
 done
-echo "Number of times Heads Won" $headsCount
-echo "Number of times Tails Won" $tailsCount
+#echo "Number of times Heads Won" $headsCount
+#echo "Number of times Tails Won" $tailsCount
